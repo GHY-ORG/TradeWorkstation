b326ef3a7cdb10ea9dd7de8505ed0bfff96c7316
cookie_xuan <cookie@ghy.cn>
2015-05-13 23:17:50 +0800

    syx add logger && enhance the security

---
+++ TradeWorkstation.Tests/Common/LoggerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Common;
+
+namespace TradeWorkstation.Tests.Common
+{
+    [TestClass]
+    public class LoggerTest
+    {
+        private ILogger logger = Logger.GetImpl();
+        [TestMethod]
+        public void TestAddLog()
+        {
+            Assert.IsTrue(logger.AddLog("系统故障~！"));
+        }
+    }
+}
